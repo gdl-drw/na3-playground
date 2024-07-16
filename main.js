b3a1860da8d109523e78da545d6b62dc3a2035d8
gdl-drw <115798661+gdl-drw@users.noreply.github.com>
2024-07-16 15:04:46 +0000

    Setup dominik demo app

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
-import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
-console.log("Hi from codespaces");
+console.log("Hi from Dominiks codespaces");
 
 // register a new route. If the user navigates to this route, your callback method will be run
 // The callback will be passed a DOM node in which the app must be rendered.
 // This sample callback will render a Svelte app into the node.
-neherapp3.registerRoute("/demo", "demo", (node) => {   
+neherapp3.registerRoute("/dominik", "dominik-demo", (node) => {   
   node.innerHTML = `
     <div>
       <a href="https://vitejs.dev" target="_blank">
         <img src="${viteLogo}" class="logo" alt="Vite logo" />
       </a>
       <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-        <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+        <img src="/javascript.svg" class="logo vanilla" alt="JavaScript logo" />
       </a>
-      <h1>Hello Vite!</h1>
+      <h1>Hello Test!</h1>
       <div class="card">
         <button id="counter" type="button"></button>
       </div>
@@ -33,9 +32,9 @@ const baseUrl = new URL(import.meta.url).origin;
 const iconUrl = baseUrl + "/vite.svg";
 // Add a new menu item to the main menu that navigates to the route you just registered
 neherapp3.addMenuItem({
-    id : "demo",
+    id : "dominik-demo",
     icon: iconUrl,
-    url: "/demo",
-    text: "Demo App",
-    parent: "settings" // this is one of "dev", "prod", "sb", "home"
+    url: "/dominik",
+    text: "Dominik Demo App",
+    parent: "dev" // this is one of "dev", "prod", "sb", "home"
 });
